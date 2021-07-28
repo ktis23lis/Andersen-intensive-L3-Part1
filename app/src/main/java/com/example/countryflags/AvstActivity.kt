0d1b9d5d7c32486d1b560c25aaecf241d3944c4b
kris23lis <kris23lis@gmail.com>
2021-07-28 11:47:33 +0300

    all files

---
+++ app/src/main/java/com/example/countryflags/AvstActivity.kt
@@ -0,0 +1,11 @@
+package com.example.countryflags
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AvstActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_avst)
+    }
+}
